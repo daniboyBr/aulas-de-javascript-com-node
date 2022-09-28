40978c95d4384d426b970ef9be45fbeb6025cc21
unknown <>
2022-09-27 23:09:57 -0300

    test: testando logica de funcao com stubs

---
+++ aula-02-stubs/src/service.js
@@ -0,0 +1,23 @@
+const https = require('https')
+const { resolve } = require('path')
+
+class Service {
+    async makeRequest(url) {
+        return new Promise((resolve, reject) => {
+            https.get(url, respose => {
+                respose.on("data", data => resolve(JSON.parse(data)))
+                respose.on("error", reject)
+            })
+        })
+    }
+    async getPlanets(url) {
+        const result = await this.makeRequest(url)
+        return {
+            name: result.name,
+            surfaceWater: result.surface_water,
+            appearedIn: result.films.length
+        }
+    }
+}
+
+module.exports = Service
